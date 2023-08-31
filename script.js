1ebf0c47d4a8ece6bed31d6385a76507daadafb9
jimoh oluwaseyi <heysey128@gmail.com>
2023-09-01 00:03:28 +0100

    Remove commented codes

--- script.js
+++ script.js
@@ -1,91 +1,91 @@
 // Menu bar
-const menu = document.querySelector(".fa-bars");
-const navMobile = document.querySelector(".navMobile");
-const cancel = document.querySelector(".fa-xmark");
+const menu = document.querySelector('.fa-bars');
+const navMobile = document.querySelector('.navMobile');
+const cancel = document.querySelector('.fa-xmark');
 
-menu.addEventListener("click", () => {
-  navMobile.style.display = "block";
-  navMobile.style.width = "100%";
-  navMobile.style.height = "97vh";
-  navMobile.style.top = "0";
-  navMobile.style.left = "0";
-  navMobile.style.backgroundColor = "#3C3A39";
+menu.addEventListener('click', () => {
+  navMobile.style.display = 'block';
+  navMobile.style.width = '100%';
+  navMobile.style.height = '97vh';
+  navMobile.style.top = '0';
+  navMobile.style.left = '0';
+  navMobile.style.backgroundColor = '#3C3A39';
 });
 
-cancel.addEventListener("click", () => {
-  navMobile.style.display = "none";
+cancel.addEventListener('click', () => {
+  navMobile.style.display = 'none';
 });
 
 // Project
 
 const projectInfo = [
   {
-    mobileName: "Multi Post Stories",
-    desktopName: "Keeping track of hundreds  of components website",
-    technology: ["HTML", "Bootstrap", "Ruby on Rails"],
-    img: "resources/Snapshoot Portfolio (1).png",
+    mobileName: 'Multi Post Stories',
+    desktopName: 'Keeping track of hundreds  of components website',
+    technology: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+    img: 'resources/Snapshoot Portfolio (1).png',
     describe:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     desktopDescribe1:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
     desktopDescribe2:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    liveLink: "#",
-    liveSource: "#",
+    liveLink: '#',
+    liveSource: '#',
   },
   {
-    desktopName: "Multi-Post Stories",
-    technology: ["CSS", "HTML", "Bootstrap", "Ruby"],
-    img: "resources/Img Placeholder.png",
+    desktopName: 'Multi-Post Stories',
+    technology: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
+    img: 'resources/Img Placeholder.png',
     describe:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
   },
   {
-    desktopName: "Profesional Art Printing Data",
-    technology: ["HTML", "Bootstrap", "Ruby"],
-    img: "resources/Img Placeholder (1).png",
-    deskImg1: "resources/Img Placeholder (3).png",
-    deskImg2: "resources/Img Placeholder (2).png",
+    desktopName: 'Profesional Art Printing Data',
+    technology: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'resources/Img Placeholder (1).png',
+    deskImg1: 'resources/Img Placeholder (3).png',
+    deskImg2: 'resources/Img Placeholder (2).png',
     describe:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
   },
   {
-    desktopName: "Data Dashboard Healthcare",
-    technology: ["HTML", "Bootstrap", "Ruby"],
-    img: "resources/Img Placeholder (1).png",
-    deskImg1: "resources/Img Placeholder (3).png",
-    deskImg2: "resources/Img Placeholder (2).png",
+    desktopName: 'Data Dashboard Healthcare',
+    technology: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'resources/Img Placeholder (1).png',
+    deskImg1: 'resources/Img Placeholder (3).png',
+    deskImg2: 'resources/Img Placeholder (2).png',
     describe:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
   },
 
   {
-    desktopName: "Website Portfolio",
-    technology: ["HTML", "Bootstrap", "Ruby"],
-    img: "resources/Img Placeholder (1).png",
-    deskImg1: "resources/Img Placeholder (3).png",
-    deskImg2: "resources/Img Placeholder (2).png",
+    desktopName: 'Website Portfolio',
+    technology: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'resources/Img Placeholder (1).png',
+    deskImg1: 'resources/Img Placeholder (3).png',
+    deskImg2: 'resources/Img Placeholder (2).png',
     describe:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
   },
 ];
 
-const ul = document.createElement("ul");
-const projectHead = document.createElement("div");
-const popupMenu = document.querySelector(".popupMenu");
-const btn = document.querySelectorAll(".btn");
-const img = document.createElement("img");
-const description = document.createElement("p");
-const description2 = document.createElement("p");
-const popupBtns = document.createElement("div");
-const popupParent = document.querySelector(".popupParent");
-const imgWrapforDesk = document.createElement("div");
-const descripDiv = document.createElement("div");
-const icon = document.createElement("i");
+const ul = document.createElement('ul');
+const projectHead = document.createElement('div');
+const popupMenu = document.querySelector('.popupMenu');
+const btn = document.querySelectorAll('.btn');
+const img = document.createElement('img');
+const description = document.createElement('p');
+const description2 = document.createElement('p');
+const popupBtns = document.createElement('div');
+const popupParent = document.querySelector('.popupParent');
+const imgWrapforDesk = document.createElement('div');
+const descripDiv = document.createElement('div');
+const icon = document.createElement('i');
 //  Class Names
-icon.className = "fa-solid fa-xmark menuCancel";
-ul.className = "menuList";
-imgWrapforDesk.className = "imgWrap";
+icon.className = 'fa-solid fa-xmark menuCancel';
+ul.className = 'menuList';
+imgWrapforDesk.className = 'imgWrap';
 //  Closing
 
 const createEl = () => {
@@ -97,12 +97,12 @@ const createEl = () => {
     projectHead.innerHTML = `<h2>${projectInfo[0].desktopName}</h2>`;
     description.innerText = projectInfo[0].desktopDescribe1;
     description2.innerText = projectInfo[0].desktopDescribe2;
-    description2.classList.add("description2");
+    description2.classList.add('description2');
     descripDiv.append(description, description2, popupBtns);
   }
   projectHead.append(icon);
   popupMenu.append(projectHead);
-  projectHead.className = "project";
+  projectHead.className = 'project';
 
   // Unorder List
   ul.innerHTML = `<li>${projectInfo[0].technology[0]}</li>
@@ -111,36 +111,36 @@ const createEl = () => {
   popupMenu.append(ul);
   img.src = projectInfo[0].img;
   descripDiv.append(description, popupBtns);
-  descripDiv.classList.add("descriptDiv");
+  descripDiv.classList.add('descriptDiv');
   imgWrapforDesk.append(img, descripDiv);
   popupMenu.append(imgWrapforDesk);
 
   popupBtns.innerHTML = `<button class="button1"><a href="${projectInfo[0].liveLink}">See live</a><img src="resources/Icon - Export.png"/></button>
                          <button class="button1"><a href="${projectInfo[0].liveSource}">See source</a><img src="resources/Icon -GitHub.png"/></button>`;
-  popupBtns.className = "popupBtn";
+  popupBtns.className = 'popupBtn';
 };
 
 //  Render the project when see Project clicked
 const showProject = () => {
   createEl();
-  popupParent.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  popupParent.style.position = "fixed";
-  popupParent.style.height = "100vh";
-  popupMenu.style.height = "80vh";
-  popupParent.style.display = "flex";
+  popupParent.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+  popupParent.style.position = 'fixed';
+  popupParent.style.height = '100vh';
+  popupMenu.style.height = '80vh';
+  popupParent.style.display = 'flex';
 };
 // Event
 btn.forEach((item) => {
-  item.addEventListener("click", showProject);
+  item.addEventListener('click', showProject);
 });
 
 //  cancel button for projects
-icon.addEventListener("click", () => {
-  popupParent.style.display = "none";
+icon.addEventListener('click', () => {
+  popupParent.style.display = 'none';
 });
 
 // showing project dynamically
-const projectOne = document.querySelector(".card1");
+const projectOne = document.querySelector('.card1');
 
 function multiPurpose() {
   projectOne.innerHTML = ` <img src="${projectInfo[1].img}" alt="" />
@@ -160,7 +160,7 @@ function multiPurpose() {
 </div>`;
 }
 multiPurpose();
-const cardParent = document.querySelector(".card2-parent");
+const cardParent = document.querySelector('.card2-parent');
 
 function mobileProject() {
   cardParent.innerHTML = ` <div class="card2">
@@ -256,7 +256,7 @@ function mobileProject() {
       </div>`;
 }
 mobileProject();
-const desktopHtml = document.querySelector(".desktopHtml");
+const desktopHtml = document.querySelector('.desktopHtml');
 
 function desktopProject() {
   desktopHtml.innerHTML = `<div class="card2-parent-desktop hide-mobile">
@@ -361,88 +361,43 @@ function desktopProject() {
 }
 desktopProject();
 
-document.addEventListener("click", (event) => {
-  if (event.target.classList.contains("btn")) {
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('btn')) {
     showProject();
   }
 });
 
-// // Form validation
-// const email = document.getElementById("email");
-// const form = document.querySelector("form");
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-// });
-
-// const submitBtn = document.querySelector(".submitBtn");
-// const small = document.querySelector("small");
-// let isValid = true;
-// submitBtn.addEventListener("click", () => {
-//   const emailValue = email.value.trim();
-//   if (emailValue !== emailValue.toLowerCase()) {
-//     small.innerText = "Email should be in lowercase";
-//     isValid = false;
-//   } else {
-//     small.innerText = "";
-//     isValid = true;
-//   }
-//   if (isValid) {
-//     form.submit();
-//   }
-// });
-// // form Storage
-// const userName = document.getElementById("name");
-// const textArea = document.getElementById("textArea");
-
-// function storeData() {
-//   let formObj = {
-//     email: email.value,
-//     userName: userName.value,
-//     textArea: textArea.value,
-//   };
-//   console.log(formObj);
-//   localStorage.setItem("data", JSON.stringify(formObj));
-//   let storeD = JSON.parse(localStorage.getItem("data"));
-// }
-
-// form.addEventListener("input", storeData);
-
-// Form validation
-
-
-
-const email = document.getElementById("email");
-const form = document.querySelector("form");
-const submitBtn = document.querySelector(".submitBtn");
-const small = document.querySelector("small");
-const userName = document.getElementById("name");
-const textArea = document.getElementById("textArea");
+const email = document.getElementById('email');
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('.submitBtn');
+const small = document.querySelector('small');
+const userName = document.getElementById('name');
+const textArea = document.getElementById('textArea');
 let isValid = true;
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener('click', () => {
   const emailValue = email.value.trim();
   if (emailValue !== emailValue.toLowerCase()) {
-    small.innerText = "Email should be in lowercase";
+    small.innerText = 'Email should be in lowercase';
     isValid = false;
   } else {
-    small.innerText = "";
+    small.innerText = '';
     isValid = true;
   }
 });
 
 function storeData() {
-  let formObj = {
+  const formObj = {
     email: email.value,
     userName: userName.value,
     textArea: textArea.value,
   };
-  localStorage.setItem("formData", JSON.stringify(formObj));
+  localStorage.setItem('formData', JSON.stringify(formObj));
 }
-form.addEventListener("input", storeData);
+form.addEventListener('input', storeData);
 
 function retrieveData() {
-  const storedData = localStorage.getItem("formData");
+  const storedData = localStorage.getItem('formData');
   if (storedData) {
     const formData = JSON.parse(storedData);
     email.value = formData.email;
@@ -451,19 +406,12 @@ function retrieveData() {
   }
 }
 
-window.addEventListener("load", retrieveData);
+window.addEventListener('load', retrieveData);
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   storeData();
   if (isValid) {
     form.submit();
   }
 });
-
-
-
-// form Storage
-
-
-
